620aa6aa17ed3c5550f7adb635711b8c724392eb
hylionx <hylia.boudahba@gmail.com>
2022-05-27 02:05:52 +0200

    home modify

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,32 +5,29 @@ import hand from "../img/hand.png";
 const Home = () => {
   return (
   <>
-  <div class="box">
-    <div class="box-content">
-      <div class="title"> Bienvenue sur le read me  <img className="waving-hand" src={hand} alt="Hand" /></div>
+  <div className="box">
+    <div className="box-content">
+      <div className="title"> Bienvenue sur le read me  <img className="waving-hand" src={hand} alt="Hand" /></div>
       <p> Ici vous trouverez tous les détails de ce qui a été codé pour vous faciliter la tâche. </p>
-      <div class="description">Mode visiteur (donc sans compte) </div>
+      <div className="description">Mode visiteur (donc sans compte) </div>
         <p> En utilisant le mode non connecté, vous pourrez consulter le planning ainsi que les manches mais uniquement en mode lecture</p>
-        <div class="description">En mode connecté</div>
-        <p>Se connecter en mode administrateur
-         Cliquer sur "LOGIN", user: admin et mdp: admin
-	       Avec le mode admin vous avez tous les droits! 
+        <div className="description">En mode connecté</div>
+        <p>Se connecter en mode administrateur  Cliquer sur "LOGIN", user: admin et mdp: admin  Avec le mode admin vous avez tous les droits! 
         </p>
-
-        <div class="grid_1_1">
-          <div class="box">
-            <div class="box-content" id="minibox">
-              <div class="title">Mes passe-temps</div>
-              <div class="description">Ce que je fais de mon temps libre...</div>
-              <div class="grid-about"></div>
+        <div className="grid_1_1">
+          <div className="box">
+            <div className="box-content" id="minibox">
+              <div className="title">Consulter la liste des personnes</div>
+              <div>Ce que je fais de mon temps libre...</div>
+              <div className="grid-about"></div>
             </div>
           </div>
-          <div class="left">
-            <div class="box">
-              <div class="box-content" id="minibox">
-                <div class="title">Mes langues parlées</div>
-                <div class="description">Même si ce n'est que des notions...</div>
-                <div class="grid-about"></div>
+          <div className="left">
+            <div className="box">
+              <div className="box-content" id="minibox">
+                <div className="title">Consulter la liste des plannings</div>
+                <div>Même si ce n'est que des notions...</div>
+                <div className="grid-about"></div>
               </div>
             </div>
           </div>
